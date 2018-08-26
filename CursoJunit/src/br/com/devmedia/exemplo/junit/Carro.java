be39f628b9e861d586403c92219c72be350eebfb
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-26 10:44:32 -0300

    criacao dos atributos da classe Carro

--- CursoJunit/src/br/com/devmedia/exemplo/junit/Carro.java
+++ CursoJunit/src/br/com/devmedia/exemplo/junit/Carro.java
@@ -1,5 +1,12 @@
 package br.com.devmedia.exemplo.junit;
 
+import java.time.LocalDate;
+
 public class Carro {
 
+	private String modelo;
+	private String marca;
+	private int anoLancamento;
+	private LocalDate dataAquisicao;
+	
 }
