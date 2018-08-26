1e4ebb5cc203b274829737fad672ca9ade13d723
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-26 10:57:17 -0300

    criacao do metodo construtor da classe Colecionador

--- CursoJunit/src/br/com/devmedia/exemplo/junit/Colecionador.java
+++ CursoJunit/src/br/com/devmedia/exemplo/junit/Colecionador.java
@@ -1,9 +1,17 @@
 package br.com.devmedia.exemplo.junit;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Colecionador {
 
 	private String nome;
 	private List<Carro> colecao;
+	
+	public Colecionador(String nome) {
+
+		this.nome = nome;
+		colecao = new ArrayList<Carro>();
+	}
+		
 }
