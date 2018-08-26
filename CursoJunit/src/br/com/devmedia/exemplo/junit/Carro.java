4c16200344c8555332f33842ce8ad66a8286247a
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-26 10:51:14 -0300

    criacao dos metodos acessores getters e setters da classe Carro

--- CursoJunit/src/br/com/devmedia/exemplo/junit/Carro.java
+++ CursoJunit/src/br/com/devmedia/exemplo/junit/Carro.java
@@ -8,11 +8,11 @@ public class Carro {
 	private String marca;
 	private int anoLancamento;
 	private LocalDate dataAquisicao;
-	
+
 	public Carro(String modelo, String marca, int anoLancamento, LocalDate dataAquisicao) {
 		super();
-		
-		if(anoLancamento > dataAquisicao.getYear()) {
+
+		if (anoLancamento > dataAquisicao.getYear()) {
 			throw new IllegalArgumentException("Ano de lançamento não pode ser maior que o ano de aquisição do carro.");
 		}
 		this.modelo = modelo;
@@ -20,7 +20,37 @@ public class Carro {
 		this.anoLancamento = anoLancamento;
 		this.dataAquisicao = dataAquisicao;
 	}
-	
-	
-	
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public LocalDate getDataAquisicao() {
+		return dataAquisicao;
+	}
+
+	public void setDataAquisicao(LocalDate dataAquisicao) {
+		this.dataAquisicao = dataAquisicao;
+	}
+
 }
