1ef75662169867ac7da32b6f8af9b0dbb387a1c1
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-26 11:04:56 -0300

    criacao do metodo obterAnoDoCarroMaisAntigo da classe Colecionador

--- CursoJunit/src/br/com/devmedia/exemplo/junit/Colecionador.java
+++ CursoJunit/src/br/com/devmedia/exemplo/junit/Colecionador.java
@@ -7,7 +7,7 @@ public class Colecionador {
 
 	private String nome;
 	private List<Carro> colecao;
-	
+
 	public Colecionador(String nome) {
 
 		this.nome = nome;
@@ -15,19 +15,32 @@ public class Colecionador {
 	}
 
 	public String getNome() {
-		
+
 		return nome;
 	}
 
 	public List<Carro> getColecao() {
-		
+
 		return colecao;
 	}
-	
+
 	public void adicionarCarro(Carro carro) {
-		
+
 		colecao.add(carro);
 	}
-	
-	
+
+	public int obterAnoDoCarroMaisAntigo() {
+
+		int maisAntigo = Integer.MAX_VALUE;
+
+		for (Carro carro : colecao) {
+
+			if (carro.getAnoLancamento() < maisAntigo) {
+
+				maisAntigo = carro.getAnoLancamento();
+			}
+		}
+		return maisAntigo;
+	}
+
 }
