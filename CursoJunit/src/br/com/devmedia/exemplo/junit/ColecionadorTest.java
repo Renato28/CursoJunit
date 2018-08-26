a87110123e3ae12f4c21457ff4b4de94e3ccde6b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-26 12:52:16 -0300

    implementacao dos testes na classe ColecionadorTest

---
+++ CursoJunit/src/br/com/devmedia/exemplo/junit/ColecionadorTest.java
@@ -0,0 +1,29 @@
+package br.com.devmedia.exemplo.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+class ColecionadorTest {
+
+	@Test
+	public void testCarroComAnoDeLancamentoMaisAntigo() {
+		
+		Colecionador devmedia = new Colecionador("Devmedia");
+		
+		Carro carro1 = new Carro("Mustang", "Ford", 1994 , LocalDate.now());
+		Carro carro2 = new Carro("Enzo", "Ferrari", 2002, LocalDate.now());
+		Carro carro3 = new Carro("Urus", "Lamborghini", 2017, LocalDate.now());
+		
+		devmedia.getColecao().add(carro1);
+		devmedia.getColecao().add(carro2);
+		devmedia.getColecao().add(carro3);
+		
+		Assert.assertEquals(1994, devmedia.obterAnoDoCarroMaisAntigo());
+	}
+
+}
